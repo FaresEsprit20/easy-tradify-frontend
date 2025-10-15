7d7ea5e02c4626556a845670918b2037f71ae168
Fares Ben Slama <fares.benslama@esprit.tn>
2025-10-15 10:38:42 +0000

    first commit

--- src/app/demos/financial-service-provider/services/services.component.ts
+++ src/app/demos/financial-service-provider/services/services.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-services',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [RouterLink],
     templateUrl: './services.component.html',
     styleUrl: './services.component.scss'
